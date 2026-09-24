14bdb94a1a6ef25c3f43e01345ab4f18b35c4318
wzy3261489869 <wzy3261489869@users.noreply.github.com>
2026-09-24 16:15:21 +0800

    feat: add PNG icon for TWA packaging + bump sw cache v18

--- website/sw.js
+++ website/sw.js
@@ -1,6 +1,6 @@
 /* 轻练 Service Worker：网络优先，离线回退缓存 + 休息结束通知兜底 */
-const CACHE = 'qinglian-v17';
-const ASSETS = ['./', './index.html', './tokens.css', './style.css', './app.js', './diet-module.js', './training-plans.js', './rewards-module.js', './manifest.webmanifest', './icon.svg', './exercises-lib.json', './foods-lib.json'];
+const CACHE = 'qinglian-v18';
+const ASSETS = ['./', './index.html', './tokens.css', './style.css', './app.js', './diet-module.js', './training-plans.js', './rewards-module.js', './manifest.webmanifest', './icon.svg', './icon-512.png', './exercises-lib.json', './foods-lib.json'];
 const restTimers = [];
 
 self.addEventListener('install', (e) => {
